b0a011c591c573ca9b80c954acbc64b39f14aacd
Georgios Varatis <varat2003@gmail.com>
2025-01-02 16:54:17 +0200

    small changes

--- MovieAnalysing.py
+++ MovieAnalysing.py
@@ -8,13 +8,13 @@ print("Dataset Preview:")
 print(ratings.head())
 
 # Filter movies with at least M ratings
-M = 50  # Replace with your desired value
+M = 50  
 movie_counts = ratings['movieId'].value_counts()
 filtered_movies = movie_counts[movie_counts >= M].index
 ratings = ratings[ratings['movieId'].isin(filtered_movies)]
 
 # Filter users with at least M' ratings
-M_prime = 20  # Replace with your desired value
+M_prime = 20  
 user_counts = ratings['userId'].value_counts()
 filtered_users = user_counts[user_counts >= M_prime].index
 ratings = ratings[ratings['userId'].isin(filtered_users)]
